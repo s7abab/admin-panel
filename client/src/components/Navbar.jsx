a53d7e7fa2ef8bcf6cc0b8070ead397edb87d7a1
SHABAB <123226241+s7abab@users.noreply.github.com>
2023-07-26 22:02:40 -0700

    second-commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,11 +17,11 @@ function Navbar() {
       <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to={"/"} className="flex items-center">
-            <img
+            {/* <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="h-8 mr-3"
               alt="Flowbite Logo"
-            />
+            /> */}
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Image Gallery
             </span>
--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -63,6 +63,7 @@ function Admin() {
         email: editEmail,
         role: editRole,
       });
+      fetchData()
       if (data.success) {
         console.log("User updated");
       } else {
@@ -70,6 +71,8 @@ function Admin() {
       }
     } catch (error) {
       console.log(error);
+    }finally{
+      setEditMode(false)
     }
   };
 
@@ -185,7 +188,8 @@ function Admin() {
         <div className="flex modal justify-center items-center">
           <div className="bg-slate-700 p-10">
             <form
-              onSubmit={() => {
+              onSubmit={(e) => {
+                e.preventDefault()
                 handleEdit(currentUser);
               }}
             >
@@ -262,7 +266,7 @@ function Admin() {
                 </div>
               </div>
               <br />
-              <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 SAVE
               </button>
             </form>
